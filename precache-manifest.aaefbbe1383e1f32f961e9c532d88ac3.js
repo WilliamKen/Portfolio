3960a64c5adfa0106a5b20b54b902fd77a0a459e
William Kendrick <williamkendrick@Williams-MacBook-Pro.local>
2024-05-29 15:03:05 -0500

    Updates

---
+++ precache-manifest.aaefbbe1383e1f32f961e9c532d88ac3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65d2f84cf433b6c6eb753c38f857233f",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "276490e05cafd2eb107d",
+    "url": "/Portfolio/static/css/main.ac1616f9.chunk.css"
+  },
+  {
+    "revision": "8a5c18ed6dfb1e180f36",
+    "url": "/Portfolio/static/js/2.0bfc5fec.chunk.js"
+  },
+  {
+    "revision": "1e86264e91bff3a75ce6187230f24c2e",
+    "url": "/Portfolio/static/js/2.0bfc5fec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9003e980e2d91588cf5",
+    "url": "/Portfolio/static/js/3.19466792.chunk.js"
+  },
+  {
+    "revision": "276490e05cafd2eb107d",
+    "url": "/Portfolio/static/js/main.1385c6bc.chunk.js"
+  },
+  {
+    "revision": "a865c944c3888953ab03",
+    "url": "/Portfolio/static/js/runtime-main.b347e779.js"
+  },
+  {
+    "revision": "7f57b9935b79741c226a60614bb44360",
+    "url": "/Portfolio/static/media/membershipBiFold-01.7f57b993.jpg"
+  },
+  {
+    "revision": "8dfd9ba5309bbb596ef57fc6398dbe00",
+    "url": "/Portfolio/static/media/membershipBiFold-02.8dfd9ba5.jpg"
+  },
+  {
+    "revision": "413e5d821788ae913b486d53af4888bd",
+    "url": "/Portfolio/static/media/membershipModel.413e5d82.png"
+  },
+  {
+    "revision": "152634bee32573073e284e80cdd948d6",
+    "url": "/Portfolio/static/media/postCard_Page_1.152634be.png"
+  },
+  {
+    "revision": "73da592ae3ec5d6892f9a5cf1047975e",
+    "url": "/Portfolio/static/media/postCard_Page_2.73da592a.png"
+  },
+  {
+    "revision": "0039a2cacba9e7947a0cbf4dd1230244",
+    "url": "/Portfolio/static/media/profile.0039a2ca.jpg"
+  },
+  {
+    "revision": "91e520d9cd0ab80a8a63b190ea4e9bae",
+    "url": "/Portfolio/static/media/values.91e520d9.png"
+  }
+]);
